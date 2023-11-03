3d0cac25d8a20eac20ab63754fab636f410ec6a7
saba-ab <sabaabjandadze@gmail.com>
2023-11-04 01:16:37 +0400

    feat: create initial App component structure
    - Set up the main layout with Container, Todo, Input, and TaskList components.
    - Add placeholder tasks in the TaskList.

---
+++ src/components/styled/TaskList.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const TaskList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border-radius: 8px;
+  background: #fff;
+  // padding: 10px;
+`;
+
+// export const Checkbox = styled.
