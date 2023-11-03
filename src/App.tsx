99d65bf58804bdcf9234caf5942cfa2065f5dad9
saba-ab <sabaabjandadze@gmail.com>
2023-11-04 03:59:11 +0400

    Enhance ToDo application:
    - Implement task addition and removal functionality.
    - Add task objects with 'isChecked' property to manage task completion status.
    - Style tasks with conditional text decoration to indicate completion.
    - Introduce and style 'Check' component to display a background image indicating task completion status.
    - Ensure random color assignment to 'Remove' button for each task and persist color on re-renders.
    - Implement hover effects to display 'DateFrom' component on task hover.
    - Refactor and fix TypeScript errors and improve type safety.
    - Update and optimize CSS and styled-components usage for better UI experience.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,60 @@ import React, { useState, ChangeEvent, KeyboardEvent } from "react";
 import { Container } from "./components/styled/Container.styled";
 import { Todo } from "./components/styled/Todo.styled";
 import { Input } from "./components/styled/Input.styled";
-import { Task, TaskList } from "./components/styled/TaskList.styled";
-interface IAppProps {}
+import {
+  Task,
+  TaskList,
+  Remove,
+  DateFrom,
+  Check,
+  TodoText,
+} from "./components/styled/TaskList.styled";
+import moment from "moment";
+import checked from "./components/checked.svg";
 
-const App: React.FC<IAppProps> = () => {
+const App: React.FC = () => {
   const [todoInput, setTodoInput] = useState<string>("");
-  const [todos, setTodos] = useState<string[]>([]);
-
+  const [todos, setTodos] = useState<TodoInt[]>([]);
+  interface TodoInt {
+    text: string;
+    isChecked: boolean;
+    postedAt: number;
+  }
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
     setTodoInput(e.target.value);
   };
 
   const handleTodo = (e: KeyboardEvent): void => {
     if (todoInput !== "" && e.key === "Enter") {
-      setTodos([todoInput, ...todos]);
+      const newTodo: TodoInt = {
+        text: todoInput,
+        isChecked: false,
+        postedAt: Date.now(),
+      };
+      setTodos([newTodo, ...todos]);
       setTodoInput("");
     }
+  };
+  const handleTimeConvertion = (time: number): string => {
+    return moment(time).fromNow();
+  };
+  const color1 = "#FF5733";
+  const color2 = "#33FF57";
+  const color3 = "#3357FF";
+  const color4 = "#FF33A1";
+  const color5 = "#A133FF";
+
+  const colors = [color1, color2, color3, color4, color5];
+
+  const handleRemove = (indexToRemove: number) => {
+    const updatedTodos = todos.filter((_, index) => index !== indexToRemove);
+    setTodos(updatedTodos);
+  };
+  const handleCheck = (indexToCheck: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos[indexToCheck].isChecked =
+      !updatedTodos[indexToCheck].isChecked;
+    setTodos(updatedTodos);
   };
 
   return (
@@ -31,7 +69,24 @@ const App: React.FC<IAppProps> = () => {
         />
         <TaskList>
           {todos.map((todo, index) => (
-            <Task key={index}>{todo}</Task>
+            <Task key={index}>
+              <Check
+                onClick={() => handleCheck(index)}
+                style={
+                  todo.isChecked ? { backgroundImage: `URL(${checked})` } : {}
+                }
+              />
+              <TodoText
+                style={todo.isChecked ? { textDecoration: "line-through" } : {}}
+              >
+                {todo.text}
+              </TodoText>
+              <DateFrom>{handleTimeConvertion(todo.postedAt)}</DateFrom>{" "}
+              <Remove
+                color={colors[index % colors.length]}
+                onClick={() => handleRemove(index)}
+              />
+            </Task>
           ))}
         </TaskList>
       </Todo>
